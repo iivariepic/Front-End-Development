20c52617a9f892d775812eaefc8527194561bb53
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-03-25 10:02:35 +0200

    Added example codes for task 2

---
+++ Assignment 5/Task 2.js
@@ -0,0 +1,4 @@
+let link = document.createElement("a");
+link.href = 'https://turkuamk.itslearning.com/'
+link.textContent = 'Go to Itslearning'
+document.getElementById('containerName').appendChild(link);
