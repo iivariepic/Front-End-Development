b97cdf3a80c31aae56602d8af440bb21a725a67e
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-03-25 13:00:00 +0200

    Task 5a complete

--- Assignment 5/MessagePage/addMessage.js
+++ Assignment 5/MessagePage/addMessage.js
@@ -1,8 +1,8 @@
 function addMessage() {
     const message = document.getElementById("newMessage").value;
-    console.log(message)
-    const newParagraph = document.createElement('p')
-    newParagraph.textContent = message
-    document.body.appendChild(newParagraph)
-    console.log(newParagraph)
+    console.log(message);
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = message;
+    document.body.appendChild(newParagraph);
+    console.log(newParagraph);
 }
