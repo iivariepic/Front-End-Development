3738322b9187a47d8cc86d820e968549b9d89f3a
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-03-25 13:00:25 +0200

    Removed console logs

--- Assignment 5/MessagePage/addMessage.js
+++ Assignment 5/MessagePage/addMessage.js
@@ -1,8 +1,6 @@
 function addMessage() {
     const message = document.getElementById("newMessage").value;
-    console.log(message);
     const newParagraph = document.createElement('p');
     newParagraph.textContent = message;
     document.body.appendChild(newParagraph);
-    console.log(newParagraph);
 }
