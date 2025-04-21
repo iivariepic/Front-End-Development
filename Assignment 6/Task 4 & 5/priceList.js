078d479d0d19c70fb9d8d31eb95daf616d66b176
Iivari Anttila <iivari03@gmail.com>
2025-04-22 01:32:08 +0300

    Task 4 complete

---
+++ Assignment 6/Task 4 & 5/priceList.js
@@ -0,0 +1,20 @@
+let priceList = [];
+
+function setPriceList(thePriceList) {
+    priceList = [...thePriceList];
+}
+
+function getPrice(itemId, quantity) {
+    const item = priceList.find(item => item.itemid === itemId)
+    if (!item) return null;
+
+    const priceBracket = item.prices
+        .sort((a, b) => b.qty - a.qty)
+        .find(bracket => quantity >= bracket.qty)
+
+    if (!priceBracket) return null;
+
+    return (quantity * priceBracket.price)
+}
+
+export { setPriceList, getPrice };
